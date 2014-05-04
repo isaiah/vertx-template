6cf4608553776403571cc2a4057046911ccf1959
isaiah <issaria@gmail.com>
2014-05-05 00:02:33 +0200

    upgrade lein-vertx to 0.3.0, bump to 0.3.0

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject vertx/lein-template "0.1.0"
   :description "leiningen template project for vertx"
   :url "http://github.com/isaiah/vertx-template"
+  :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
